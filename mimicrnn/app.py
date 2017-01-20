06eda9b6ee5e44d2f4dd41fc2316b0c062489379
JordanCoblin <j_coblin@hotmail.com>
2017-01-20 00:55:27 -0800

    Got celery working with redis to run code in a background task, also added a requirements.txt and gitignore

--- mimicrnn/app.py
+++ mimicrnn/app.py
@@ -1,12 +1,40 @@
 #!C:\Users\Jordan_2\Documents\Programming\Onomastic\Scripts\python
 
 from flask import Flask, render_template
-import keras_rnn
+from celery import Celery
+#import keras_rnn
+
 app = Flask(__name__)
+
+def make_celery(flask_app):
+    celery = Celery(app.import_name, backend=flask_app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(flask_app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with flask_app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+# configure app to use celery
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(app)
 
 @app.route("/")
 def main():
+	rnn_task = train_rnn.delay()
+	print('created celery task')
 	return render_template('index.html')
+
+@celery.task()
+def train_rnn():
+    print('woah its printing :O')
 
 if __name__ == "__main__":
 	app.run()
